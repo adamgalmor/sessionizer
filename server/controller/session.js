689df8b1bbb1d44847653fe7dc91e623b4e2143e
Adam <adamgalmor@gmail.com>
2020-02-09 12:31:07 +0200

    Migrated to async await syntax

--- server/controller/session.js
+++ server/controller/session.js
@@ -10,57 +10,53 @@ var generate_key = function() {
 /**
  * Function to collect session data.
  */
-exports.collect = function (req, res, next) {
+exports.collect = (req, res) => {
+    //if we get a cookie
     if(req.cookies['sessionId']){
         let query = { sessionId: req.cookies['sessionId'] };
 
-        //if we get a cookie, try to find the session record in the DB
-        sessionService.findSession(query, function (error, result) {
-            if (error) {
-                res.status(404).send(error);
-                return;
-            }
+        //try to find the session record in the DB
+        sessionService.findSession(query).then(result => {
             if (result) {
                 //if the session record is found in the DB, go to the sessionFoundHandler and check for the session TTL (experation) 
                 sessionFoundHandler(req, res, result);
             }
-            if (!result) {
+            else if(!result) {
                 //if the session record is not found in the DB, (because of the cleaning process who removes expired sessions) then create a new session record in the DB
                 sessionNotFoundHandler(req, res);
             }
+        }).catch(error => {
+            res.status(400).send(error);
         });
     }
+    //else means we didnt get any sessionId value from the cilent, either because it expired or this is the first time the user connect
     else{
         sessionNotFoundHandler(req, res);
     }
 
 }
 
-var sessionFoundHandler = function(req, res, sessionContext){
+var sessionFoundHandler = (req, res, sessionContext) => {
     //if the session expired create a new SID else use the same one, then update the data. (always update the TTL):
     let session = Date.now() > sessionContext.ttl ? new Session(req.query) : new Session(req.query, sessionContext.sessionId);
 
-    sessionService.updateSessionById(sessionContext.id, session, (err, response) => {
-        if (response) {
-            res.cookie('sessionId', session.sessionId, { expires: session.ttl, httpOnly: true });
-            res.status(201).send(response);
-        } else if (err) {
-            res.status(400).send(err);
-        }
+    sessionService.updateSessionById(sessionContext.id, session).then(result => {
+        res.cookie('sessionId', session.sessionId, { expires: session.ttl, httpOnly: true });
+        res.status(201).send(result);        
+    }).catch(error => {
+        res.status(400).send(error);
     });
 }
 
-var sessionNotFoundHandler = function(req, res){
+var sessionNotFoundHandler = (req, res) => {
     //create a new SID, save the data and send the client a coockie with the SID
     let session = new Session(req.query);
     
-    sessionService.collectSession(session, function (error, response) {
-        if (response) {
-            res.cookie('sessionId',session.sessionId, { expires: session.ttl, httpOnly: true });
-            res.status(201).send(response);
-        } else if (error) {
-            res.status(400).send(error);
-        }
+    sessionService.collectSession(session).then(result => {
+        res.cookie('sessionId',session.sessionId, { expires: session.ttl, httpOnly: true });
+        res.status(201).send(result);        
+    }).catch(error =>{
+        res.status(400).send(error);
     });
 }
 
--- server/service/session.js
+++ server/service/session.js
@@ -1,29 +1,34 @@
-(function () {
-    var mongoose = require('mongoose');
-    var session = mongoose.model('Session');
-    /**
-     * Function to execute the create query to save the session data in MongoDB.
-     * This is done so that other nodes\proccess can retrieve an manipulate sessions accross the swarm
-     * @param {*} data session data
-     * @param {*} callback callback function.
-     */
-    exports.collectSession = function (data, callback) {
-        session.create(data).then((response) => {
-            callback(null, response);
-        }, (error) => {
-            callback(error, null);
-        });
-    };
+var mongoose = require('mongoose');
+var session = mongoose.model('Session');
 
-    exports.updateSessionById = function (id, data, callback) {
-        session.findByIdAndUpdate({
-            _id: id
-        }, data, (err, response) => {
-            callback(err, response);
-        });
-    }
+module.exports = {
+    collectSession,
+    findSession,
+    updateSessionById
+};
 
-    exports.findSession = function (query, callback) {
-        session.findOne(query, callback);
-    }    
-})()
+/**
+ * Execute the create query in order to save the session data in MongoDB.
+ * This is done so that other nodes\proccess can retrieve an manipulate sessions accross all nodes
+ * @param {*} data session data
+ */
+async function collectSession(data) {
+    return await session.create(data);
+}
+
+/**
+ * Function to find a specific session by the DB record ID and update it's data (the query strings payload)
+ * @param {*} id session record id (not the sessionId)
+ * @param {*} data session data
+ */
+async function updateSessionById(id, data){
+    return session.findByIdAndUpdate({ _id: id }, data);
+}
+
+/**
+ * Function to find a specific session by the sessionId sent from the client's sessionId coockie value
+ * @param {*} sessionId session id from the coockie value
+ */
+async function findSession(sessionId) {
+    return await session.findOne(sessionId);
+}
